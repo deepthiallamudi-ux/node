2920adc25404b0eac1096324a04d2d4a2ee8ccf8
sarat45 <mrsaratchandra216@gmail.com>
2026-01-17 15:49:10 +0530

    Clean up express.js by removing comments
    Removed commented-out API endpoint documentation.

--- Middleware-assign/express.js
+++ Middleware-assign/express.js
@@ -15,21 +15,3 @@ app.use('/todos', todoRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-
-// POST http://localhost:3000/todos/add
-// Body: { "title": "Learn Express" }
-
-// # Get all todos (limited to 15 per minute)
-// GET http://localhost:3000/todos
-
-// # Get single todo
-// GET http://localhost:3000/todos/1
-
-// # Update a todo
-// PUT http://localhost:3000/todos/update/1
-// Body: { "title": "Updated title", "completed": true }
-
-// # Delete a todo
-// DELETE http://localhost:3000/todos/delete/1
